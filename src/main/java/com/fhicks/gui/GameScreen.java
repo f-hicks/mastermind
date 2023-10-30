473083295a82e79d109895eeb1847f24ab90874e
f-hicks <84518177+f-hicks@users.noreply.github.com>
2023-10-30 12:15:12 +0000

    use util class for handling cell color renders

--- src/main/java/com/fhicks/gui/GameScreen.java
+++ src/main/java/com/fhicks/gui/GameScreen.java
@@ -1,8 +1,11 @@
 package com.fhicks.gui;
 
+import com.fhicks.util.CustomCellRenderer;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -15,13 +18,12 @@ public class GameScreen extends JFrame implements ActionListener {
     private DefaultTableModel tableModel;
 
     private int numDigits;
-    private int numGuesses;
     private int currentGuess = 0;
     private ArrayList<Integer> secretCode = new ArrayList<>();
+    JTextField textBox = new JTextField();
 
     public GameScreen(int numDigits, int numGuesses, boolean easyMode, String num) {
         this.numDigits = numDigits;
-        this.numGuesses = numGuesses;
 
         for (char c : num.toCharArray()) {
             secretCode.add(Character.getNumericValue(c));
@@ -34,40 +36,12 @@ public class GameScreen extends JFrame implements ActionListener {
         button.addActionListener(this);
 
         // Create a table with a DefaultTableModel
-        tableModel = new DefaultTableModel(numGuesses, numDigits);
+        tableModel = new DefaultTableModel(10, numDigits);
         table = new JTable(tableModel);
         table.setTableHeader(null);
+        table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer());
+        table.setValueAt("a string", 0, 0);
 
-        for (int i = 0; i < numGuesses; i++) {
-            tableModel.setValueAt(i + 1, i, 0); // Set row numbers
-        }
-
-        // Set up renderer to color the cells
-        DefaultTableCellRenderer cellRenderer = new DefaultTableCellRenderer() {
-            @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-                Component comp = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                int guess = row;
-                if (guess < currentGuess) {
-                    int digit = (int) tableModel.getValueAt(row, column);
-                    if (digit == secretCode.get(column - 1)) {
-                        comp.setBackground(Color.GREEN);
-                    } else if (secretCode.contains(digit)) {
-                        comp.setBackground(Color.YELLOW);
-                    } else {
-                        comp.setBackground(Color.WHITE);
-                    }
-                } else {
-                    comp.setBackground(Color.WHITE);
-                }
-                return comp;
-            }
-        };
-
-        // Apply renderer to all cells except the first column
-        for (int i = 0; i <= numDigits-1; i++) {
-            table.getColumnModel().getColumn(i).setCellRenderer(cellRenderer);
-        }
 
         panel.add(textField);
         panel.add(button);
@@ -90,16 +64,34 @@ public class GameScreen extends JFrame implements ActionListener {
             // Store the guess in the table
             for (int i = 0; i < numDigits; i++) {
                 int digit = Character.getNumericValue(input.charAt(i));
-                System.out.println(digit);
-                tableModel.setValueAt(digit, -1, -1);
+                tableModel.setValueAt(digit, currentGuess, i);
+                if (digit == secretCode.get(i)) {
+                    table.getColumnModel().getColumn(i).setCellRenderer(new CustomCellRenderer(currentGuess, i, Color.GREEN));
+                } else if (secretCode.contains(digit)) {
+                    table.getColumnModel().getColumn(i).setCellRenderer(new CustomCellRenderer(currentGuess, i, Color.YELLOW));
+                } else {
+                    table.getColumnModel().getColumn(i).setCellRenderer(new CustomCellRenderer(currentGuess, i, Color.RED));
+                }
+                
             }
-
             currentGuess++;
 
-            if (currentGuess == numGuesses) {
+
+            //check if guess is correct
+            System.out.println(input + " " + secretCode.toString().replaceAll("[\\[\\], ]", ""));
+            if (input.equals(secretCode.toString().replaceAll("[\\[\\], ]", ""))) {
+                // Handle end of the game (correct guess)
+                JOptionPane.showMessageDialog(this, "Correct guess!\n It took you " + currentGuess + " guesses.");
+                dispose();
+                System.exit(0);
+            }
+
+
+            if (currentGuess == 10) {
                 // Handle end of the game (no more guesses)
                 JOptionPane.showMessageDialog(this, "Out of guesses. The secret code was: " + secretCode.toString());
                 dispose();
+                System.exit(1);
             } else {
                 textField.setText("");
                 table.repaint();
